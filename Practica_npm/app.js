9471019793970671149a7d912263ce688df12ad3
javier <javierverduzco13@gmail.com>
2022-04-27 08:42:51 -0500

    Practica en clase

--- Practica_npm/app.js
+++ Practica_npm/app.js
@@ -1,12 +1,37 @@
+/* NODE
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("respuesta del servidor...");
-  res.end("Te envío un saludo desde el servidor con node.js");
+  res.end("Te envio un saludo desde el servidor con node.js v2");
 });
 
 const puerto = 3001;
 
 server.listen(puerto, () => {
   console.log("Escuchando...");
+});
+*/
+
+// EXPRESS
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/1", (req, res) => {
+  res.send("AAAAA");
+});
+
+app.get("/contacto", (req, res) => {
+  res.send("Hola");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
