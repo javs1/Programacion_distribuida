4c594113492ecd5bc84007f350292f892a642feb
javier <javierverduzco13@gmail.com>
2022-05-04 08:53:32 -0500

    Practica ejs

--- Practica_npm/app.js
+++ Practica_npm/app.js
@@ -18,18 +18,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/views"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", { titulo: "inicio EJS" });
 });
 
-app.get("/1", (req, res) => {
-  res.send("AAAAA");
+app.get("/servicios", (req, res) => {
+  res.render("servicios", { titulo: "Servicios EJS"});
 });
 
 app.get("/contacto", (req, res) => {
-  res.send("Hola");
+  res.render("contacto", { titulo: "Nosotros EJS"});
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { titulo: "Página 404", tituloweb: 'Error EJS' });
 });
 
 app.listen(port, () => {
