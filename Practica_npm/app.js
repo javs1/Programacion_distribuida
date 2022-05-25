8792542c20998efbd937503c0a4713f9841bbc33
javier <javierverduzco13@gmail.com>
2022-05-25 08:43:23 -0500

    Añadido mongodb

--- Practica_npm/app.js
+++ Practica_npm/app.js
@@ -17,6 +17,16 @@ server.listen(puerto, () => {
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+const usr = 'jav';
+const pass = 'rojo123rojo';
+const dbname = 'Cluster0';
+const uri = `mongodb+srv://${usr}:${pass}@${dbname}.3kewh.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> console.log('conectado a mongodb')) 
+  .catch(e => console.log('error de conexión', e))
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
